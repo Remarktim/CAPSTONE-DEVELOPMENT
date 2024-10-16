c6bcdba1ca238c53412710c68d4d71fbe93c17bc
Nick Francis Bundal <nnikeetmg@gmail.com>
2024-10-17 00:08:13 +0800

    officer CRUD

--- GPPG/pangolin/models.py
+++ GPPG/pangolin/models.py
@@ -116,6 +116,7 @@ class Officer(BaseModel):
     last_name = models.CharField(max_length=150)
     date_joined = models.DateField()
     position = models.CharField(max_length=150)
+    officer_image = models.ImageField(upload_to='media/', null=True, blank=True)
 
     def __str__(self):
         return f"{self.last_name}, {self.first_name}"
